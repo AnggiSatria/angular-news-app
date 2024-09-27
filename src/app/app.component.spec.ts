7c5eb0500d563af88e6700cc2a4fa05ff79402ef
AnggiSatria <anggi.satria@talentinsider.com>
2024-09-27 19:18:26 +0700

    [Anggi]: section add first repo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'angular-news-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-news-app');
+    expect(app.title).toEqual('World');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-news-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello world');
   });
 });
